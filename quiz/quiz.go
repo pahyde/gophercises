c5a5714b055e89465768209f9e4ea33999e7a1a9
Parker Hyde <parkerhyde79@gmail.com>
2022-09-15 20:46:29 -0400

    quiz timer

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -2,58 +2,131 @@ package main
 
 import (
     "fmt"
-    "log"
+    "strings"
     "os"
-    "io"
     "flag"
     "encoding/csv"
+    "time"
 )
 
-func main() {
-    total   := 0
-    correct := 0
+type Problem struct {
+    q string
+    a string
+}
 
-    filename := flag.String(
+type Answer struct {
+    isCorrect bool
+}
+
+func main() {
+
+    filename := *flag.String(
         "csv", 
         "problems.csv", 
         "a csv file in the format (question, answer). defaults to problems.csv",
     )
+    seconds := *flag.Int(
+        "time", 
+        16,
+        "the time limit for the quiz (in seconds). defaults to 16s",
+    )
     flag.Parse()
 
-    file, err := os.Open(*filename)
+    file, err := os.Open(filename)
     if err != nil {
-        log.Fatal(err)
+        exit(fmt.Sprintf("Failed to open file: %s!", filename))
     }
 
     r := csv.NewReader(file)
-    fmt.Println("Ready, set, Go! (pun intended)")
-    for {
-        entry, err := r.Read()
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            log.Fatal(err)
-        }
-        if len(entry) != 2 {
-            err := fmt.Errorf("quiz question %d is formatted incorrectly. Must contain a question and answer column only.", len(entry))
-            log.Fatal(err)
-        }
-
-        question   := entry[0]
-        answer     := entry[1]
-        userAnswer := ""
-
-        fmt.Printf("%s : ", question)
-        _, err = fmt.Scanln(&userAnswer)
-        if err != nil {
-            log.Fatal(err)
-        }
-        if userAnswer == answer {
-            correct++
-        }
-        total++
+    lines, err := r.ReadAll()
+    if err != nil {
+        exit(fmt.Sprintf("Something went wrong reading %s.", filename))
     }
 
-    fmt.Printf("Nice, you answered %d/%d correctly\n", correct, total)
+    problems, err := parseProblems(lines)
+    if err != nil {
+        exit(err.Error())
+    }
+
+    ansCh  := make(chan Answer)
+    quitCh := make(chan int)
+    
+    // handles state changes (e.g. submitted answers and time limit reached)
+    go handleState(ansCh, quitCh, len(problems))
+
+    // start timer
+    // note: time.NewTimer might be a better choice here
+    go func() {
+        time.Sleep(time.Duration(seconds) * time.Second)
+        quitCh <- 0
+    }()
+
+    // start quiz
+    fmt.Println("Ready, set, Go! (pun intended)")
+    for _, p := range problems {
+        answer, err := getUserAnswer(p)
+        if err != nil {
+            exit("Something went wrong reading your answer. Please try again.")
+        }
+        ansCh <- Answer{isCorrect: answer == p.a}
+    }
+    fmt.Print("\nNice job!")
+    quitCh <- 0
+}
+
+func handleState(ansCh chan Answer, quitCh chan int, total int) {
+    correct  := 0
+    answered := 0
+    for {
+        select {
+        case a := <-ansCh:
+            answered++
+            if a.isCorrect {
+                correct++
+            }
+        case <-quitCh:
+            displayResults(correct, answered, total)
+            return
+        }
+    }
+}
+
+func displayResults(correct, answered, total int) {
+    score := 0.0
+    if answered > 0 {
+        score = float64(correct) / float64(answered) * 100
+    }
+    template := "\n\nYou answered %d out of %d questions and got %d/%d (%.2f%%) correct\n\n"
+    fmt.Printf(template, correct, total, correct, answered, score)
+    os.Exit(0)
+}
+
+func getUserAnswer(p Problem) (string, error) {
+    fmt.Printf("%s = ", p.q)
+    var answer string
+    _, err := fmt.Scanln(&answer)
+    if err != nil {
+        return answer, err
+    }
+    return answer, nil
+}
+
+func parseProblems(lines [][]string) ([]Problem, error) {
+    problems := make([]Problem, len(lines))
+    for i, line := range lines {
+        if len(line) != 2 {
+            return nil, fmt.Errorf("quiz question %d is formatted incorrectly. Must contain a question and answer column only.", i)
+        }
+        p := Problem{
+            q: line[0],
+            a: strings.TrimSpace(line[1]),
+        }
+        problems[i] = p
+    }
+    return problems, nil
+}
+
+func exit(m string) {
+    fmt.Println(m)
+    os.Exit(1)
 }
