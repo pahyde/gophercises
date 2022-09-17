390a546006142c6a5155c317196b883a3c2b8efd
Parker Hyde <parkerhyde79@gmail.com>
2022-09-16 23:54:51 -0400

    urlshort

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -2,6 +2,11 @@ package urlshort
 
 import (
 	"net/http"
+	"net/url"
+	"log"
+	"gopkg.in/yaml.v3"
+	"encoding/json"
+	"encoding/xml"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -11,8 +16,25 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+    return func(w http.ResponseWriter, r *http.Request) {
+        urlstr, ok := pathsToUrls[r.URL.Path]
+        if !ok {
+            // short url not found
+            fallback.ServeHTTP(w,r)
+            return
+        }
+        dest, err := url.Parse(urlstr)
+        if err != nil {
+            // error parsing long url
+            log.Printf("error parsing long url %s. possibly invalid format.\n", urlstr)
+            fallback.ServeHTTP(w,r)
+            return
+        }
+        // update url host and path, preserve all else (query params etc)
+        r.URL.Host = dest.Host
+        r.URL.Path = dest.Path
+        http.Redirect(w, r, r.URL.String(), 302)
+    }
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -31,7 +53,86 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
+
+// supported serialization formats
+type fileType int
+const (
+    jsonf   fileType = iota
+    yamlf
+    xmlf
+)
+
+// PathToUrl: data structure representing a short path to long url kv-pair
+// yaml, json, and xml formats must have underlying []PathToUrl structure
+type PathToUrl struct {
+    Path string
+    Url  string
+}
+
+// PathToUrl wrapper for unmarshaling xml
+type PathsToUrlsXML struct {
+    List []PathToUrl `xml:"PathToUrl"`
+}
+
+func parseYAML(yml []byte) ([]PathToUrl, error) {
+    var l []PathToUrl
+    if err := yaml.Unmarshal(yml, &l); err != nil {
+        return l, err
+    }
+    return l, nil
+}
+
+func parseJSON(jsn []byte) ([]PathToUrl, error) {
+    var l []PathToUrl
+    if err := json.Unmarshal(jsn, &l); err != nil {
+        return l, err
+    }
+    return l, nil
+}
+
+func parseXML(xm []byte) ([]PathToUrl, error) {
+    var l PathsToUrlsXML
+    if err := xml.Unmarshal(xm, &l); err != nil {
+        return nil, err
+    }
+    return l.List, nil
+}
+
+func buildMap(l []PathToUrl) map[string]string {
+    m := make(map[string]string)
+    for _, entry := range l {
+        m[entry.Path] = entry.Url
+    }
+    return m
+}
+
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+    return convertToMapHandler(yml, fallback, yamlf)
+}
+
+func JSONHandler(jsn []byte, fallback http.Handler) (http.HandlerFunc, error) {
+    return convertToMapHandler(jsn, fallback, jsonf)
+}
+
+func XMLHandler(xm []byte, fallback http.Handler) (http.HandlerFunc, error) {
+    return convertToMapHandler(xm, fallback, xmlf)
+}
+
+func convertToMapHandler(dat []byte, fallback http.Handler, f fileType) (http.HandlerFunc, error) {
+    var parsed []PathToUrl
+    var err error
+    switch f {
+    case yamlf:
+        parsed, err = parseYAML(dat)
+    case jsonf:
+        parsed, err = parseJSON(dat)
+    case xmlf:
+        parsed, err = parseXML(dat)
+    }
+    if err != nil {
+        return nil, err
+    }
+    pathMap := buildMap(parsed)
+    log.Println(pathMap)
+    return MapHandler(pathMap, fallback), nil
 }
--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -2,13 +2,56 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"net/http"
+	"path/filepath"
+	"flag"
 
-	"github.com/gophercises/urlshort"
+	"urlshort"
 )
 
 func main() {
 	mux := defaultMux()
+
+    // file flag: Allows user to submit addition path-url redirection pairs
+    // in either yaml, json, or xml format
+    m := `Input yaml, json, or xml file denoting key-value (path-url)
+redirection pairs for url shortener`
+    inputFile := *flag.String("file", "", m)
+    flag.Parse()
+
+    fmt.Println(inputFile)
+    // yaml, json, and xml default values
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+    json := `
+[
+    {
+        "path": "/fall",
+        "url": "https://youtu.be/QhBoFEq0lU0"
+    },
+    {
+        "path": "/feels",
+        "url": "https://soundcloud.com/makzo/shanti"
+    }
+]
+    `
+    xml := `
+<PathsToUrls>
+    <PathToUrl>
+        <Path>/my-gophercises</Path>
+        <Url>https://github.com/pahyde/gophercises</Url>
+    </PathToUrl>
+    <PathToUrl>
+        <Path>/my-urlshort</Path>
+        <Url>https://github.com/pahyde/gophercises/urlshort</Url>
+    </PathToUrl>
+</PathsToUrls>
+    `
 
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
@@ -17,20 +60,58 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
+
+	// Build the XMLHandler -> JSONHandler -> YAMLHandler -> mapHandler as the fallback
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
 	if err != nil {
 		panic(err)
 	}
+    jsonHandler, err := urlshort.JSONHandler([]byte(json), yamlHandler)
+	if err != nil {
+	    fmt.Println(json)
+		panic(err)
+	}
+	xmlHandler, err := urlshort.XMLHandler([]byte(xml), jsonHandler)
+	if err != nil {
+		panic(err)
+	}
+
+    entryPoint := xmlHandler
+    if inputFile != "" {
+        h, err := getHandlerFromFile(inputFile, xmlHandler)
+        if err != nil {
+            exit(err)
+        }
+        entryPoint = h
+    }
+
+	// start the server
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", entryPoint)
+}
+
+func getHandlerFromFile(filename string, fallback http.Handler) (http.HandlerFunc, error) {
+    b, err := os.ReadFile(filename)
+    if err != nil {
+        return nil, fmt.Errorf("Unable to read file %s", filename)
+    }
+    ext := filepath.Ext(filename)
+    switch ext {
+    case ".yaml":
+        return urlshort.YAMLHandler(b, fallback)
+    case ".json":
+        return urlshort.JSONHandler(b, fallback)
+    case ".xml":
+        return urlshort.XMLHandler(b, fallback)
+    default:
+        return nil, fmt.Errorf("Invalid file extention: %s\nMust be json, yaml, or xml.", ext)
+    }
+
+}
+
+func exit(m any) {
+    fmt.Println(m)
+    os.Exit(1)
 }
 
 func defaultMux() *http.ServeMux {
